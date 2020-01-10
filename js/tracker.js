339079b8d93e1d76d597c8e6382e1823e4dfc4c6
Serenity911 <silvia.simonassi01@gmail.com>
2020-01-10 17:09:48 +0000

    dynamically populateSeasonList

--- js/tracker.js
+++ js/tracker.js
@@ -4,22 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const formInputs = document.querySelector('#inputForm');
   formInputs.addEventListener('submit', handleFormSubmitted);
 
-  // const watchedList = document.querySelector('.list');
-  // console.log(watchedList);
-  // watchedList.addEventListener('submit', handleWatchedList);
+  const clearButton = document.querySelector('#delete-button');
+  clearButton.addEventListener('click', handleClearButton);
+
+  const seriesSelected = document.querySelector('#series');
+  seriesSelected.addEventListener('change', populateSeasonList )
 });
 
 
 const handleFormSubmitted = function () {
+  // get inputs from form
   const readingFormInputs = getFormInputs(event.target);
-  console.log('readingFormInputs', readingFormInputs);
+  // create a new list
   const getNewListItem = createListItem();
-  console.log('getNewListItem', getNewListItem);
-  const createNewTags = createSeriesNameTag(readingFormInputs);
-  console.log('createNewTags', createNewTags);
+  // create new html tags
+  const newSeriesTag = createSeriesNameTag(readingFormInputs);
+  const newSeasonTag = createSeasonTag(readingFormInputs);
+  const newEpisodeTag = createEpisodeTag(readingFormInputs);
+  const newNoteTag = createNoteTag(readingFormInputs);
 
-    getNewListItem.appendChild(createNewTags);
   // add it to DOM
+  addNewTag(getNewListItem, newSeriesTag);
+  addNewTag(getNewListItem, newSeasonTag);
+  addNewTag(getNewListItem, newEpisodeTag);
+  addNewTag(getNewListItem, newNoteTag);
+  event.target.reset();
+
 };
 
 const getFormInputs = function () {
@@ -44,4 +54,50 @@ const createSeriesNameTag = function (readingFormInputs) {
   const seriesName = document.createElement('h3');
   seriesName.textContent = readingFormInputs['series'];
   return seriesName;
+}
+
+const createSeasonTag = function (readingFormInputs) {
+  const season = document.createElement('h4');
+  season.textContent = readingFormInputs['season'];
+  return season;
+}
+
+const createEpisodeTag = function (readingFormInputs) {
+  const episode = document.createElement('p');
+  episode.textContent = readingFormInputs['episode'];
+  return episode;
+}
+
+const createNoteTag = function (readingFormInputs) {
+  const note = document.createElement('p');
+  note.textContent = readingFormInputs['note'];
+  return note;
+}
+
+const addNewTag = function (getNewListItem, newTagToAdd) {
+  getNewListItem.appendChild(newTagToAdd);
+}
+
+const handleClearButton = function () {
+  const list = document.querySelector('.list');
+  list.innerHTML = "";
+  console.log('handleClearButton', handleClearButton);
+}
+
+const populateSeasonList = function () {
+  const season = {"Doctor Who": 12, "Game of Throne": 8, "Peaky Blinders": 5};
+  selectedSeries = event.target.value;
+  seasonsNumber = season[selectedSeries];
+  console.log("selectedSeries", selectedSeries);
+  const seasonList = document.querySelector('#season');
+  seasonList.innerHTML = "";
+
+  for (i = 1; i <= seasonsNumber; i++) {
+    const seasonList = document.querySelector('#season');
+    let itemList = document.createElement('option');
+    itemList.value = i;
+    console.log("i", i);
+    itemList.textContent = i;
+    seasonList.appendChild(itemList);
+  }
 }
--- weekend_hw_dom.md
+++ weekend_hw_dom.md
@@ -13,10 +13,10 @@ Your task is to create a front-end JavaScript application that allows users to i
 ### MVP
 
 -> Create a form in HTML with inputs for relevant data
-- When the form is submitted, access the data from the form in the form's submit `event` object
-- Create a list in HTML
-- Append the submitted data to the list
-- Add a "Delete All" `<button>` which clears all of the list items from the list
+-> When the form is submitted, access the data from the form in the form's submit `event` object
+-> Create a list in HTML
+-> Append the submitted data to the list
+-> Add a "Delete All" `<button>` which clears all of the list items from the list
 
 Note: If you are at all unclear about the process of reading and writing to the DOM, complete the MVP, writing all the code in one function. Understanding the `document`'s methods is the main learning for this homework. Abstracting code into helper functions is secondary.
 
